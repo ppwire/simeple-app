af52d9d79c02ee8efdd11df828eaa1d630661bc3
ppwire <poom.mth@gmail.com>
2021-07-20 21:02:28 +0700

    test frontend env

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <Header></Header>
   <router-view />
 </template>
+
+<script>
+import Header from "./components/Header.vue";
+import 'primeflex/primeflex.css';
+
+export default {
+  components:{
+    Header
+  },
+  setup() {
+    
+  },
+};
+</script>
 
 <style>
 #app {
@@ -13,18 +24,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
